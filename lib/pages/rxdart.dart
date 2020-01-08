3ad6000a97c74ee9b1a63e2ad1455e53cc80fbc4
WingCH <g1191993@study.ouhk.edu.hk>
2020-01-08 22:31:22 +0800

    init rxdart page

--- lib/pages/rxdart.dart
+++ lib/pages/rxdart.dart
@@ -7,14 +7,6 @@ class RxDartDemo extends StatefulWidget {
 
 class RxDartDemoState extends State<RxDartDemo> {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(''),
-      ),
-    );
-  }
-  @override
   void initState() {
     // TODO: implement initState
     super.initState();
@@ -36,5 +28,143 @@ class RxDartDemoState extends State<RxDartDemo> {
   void didChangeDependencies() {
     // TODO: implement didChangeDependencies
     super.didChangeDependencies();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Stream'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 30),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                    width: 300,
+                    child: FittedBox(
+                        fit: BoxFit.scaleDown,
+                        child: SizedBox(
+                            child: Text(
+                          'Dxdart Demo',
+                          style: TextStyle(
+                              fontSize: 60,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.teal),
+                        )))),
+              ],
+            ),
+          ),
+          Padding(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                children: <Widget>[
+                  Text(
+                    "00",
+                    style: TextStyle(
+                        color: Colors.blueGrey,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 80),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      OutlineButton(
+                        onPressed: () {
+                          //time 1 press
+                        },
+                        child: Text('Listen'),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                        child: OutlineButton(
+                          onPressed: () {},
+                          child: Text('Paused'),
+                        ),
+                      ),
+                      OutlineButton(
+                        onPressed: () {},
+                        child: Text('Resume'),
+                      ),
+                    ],
+                  )
+                ],
+              )),
+          Padding(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                children: <Widget>[
+                  Text(
+                    "00",
+                    style: TextStyle(
+                        color: Colors.amber,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 80),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      OutlineButton(
+                        onPressed: () {
+                          //time 2 press
+                        },
+                        child: Text('Listen'),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                        child: OutlineButton(
+                          onPressed: () {},
+                          child: Text('Paused'),
+                        ),
+                      ),
+                      OutlineButton(
+                        onPressed: () {},
+                        child: Text('Resume'),
+                      ),
+                    ],
+                  )
+                ],
+              )),
+          Padding(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                children: <Widget>[
+                  Text(
+                    "00",
+                    style: TextStyle(
+                        color: Colors.deepOrange,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 80),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      OutlineButton(
+                        onPressed: () {
+                          //time3 press
+                        },
+                        child: Text('Listen'),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                        child: OutlineButton(
+                          onPressed: () {},
+                          child: Text('Paused'),
+                        ),
+                      ),
+                      OutlineButton(
+                        onPressed: () {},
+                        child: Text('Resume'),
+                      ),
+                    ],
+                  )
+                ],
+              ))
+        ],
+      ),
+    );
   }
 }
